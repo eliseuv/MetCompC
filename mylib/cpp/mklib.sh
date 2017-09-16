2cf001d4f4b3a78cb2ab1de0a1c1ccffeac0e8dc
drfreeman816 <eliseuv@live.com>
2017-09-16 20:04:52 -0300

    Modified shell script

--- mylib/cpp/mklib.sh
+++ mylib/cpp/mklib.sh
@@ -2,11 +2,12 @@
 
 # My cpp lib make script
 
-# Create library
+# Compile library to object code
 g++ -std=c++11 -I ./inc -c ./src/mylib.cpp -o ./obj/mylib.o
+# Generate library file
 ar rcs ./lib/libMetComp.a ./obj/mylib.o
 
-# Use library
+# Use library in test program
 g++ -std=c++11 -I ./inc -L ./lib -static main.cpp -lMetComp -o ./bin/main
 
 # Run test program
